f497079c8a93a9c08251962132797ba9a8cfa0a6
Kathryn Stapleton <kathryn.stapleton777@gmail.com>
2018-07-10 15:43:04 -0400

    add a switch for links w default case

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
@@ -9,6 +11,19 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1 className="App-title">api parTAy</h1>
         </header>
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">gitHub API</NavLink>
+          </li>
+        
+        </ul>
+
+        <Switch>
+          <Route path="/github" render={() => <h1>Github</h1>}/>
+          <Route render={() => (
+            <p>To get started, click on one of the links above</p>
+          )}/>
+        </Switch>
       </div>
     );
   }
