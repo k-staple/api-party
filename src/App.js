7cfee4f6991b3ca91fc7a84a97c5bcf681b1565d
Kathryn Stapleton <kathryn.stapleton777@gmail.com>
2018-07-10 15:34:54 -0400

    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">     
+      <div className="App">   
+        <header>  
+          <h3>Ain't no party like an</h3>
           <h1 className="App-title">api parTAy</h1>
+        </header>
       </div>
     );
   }
